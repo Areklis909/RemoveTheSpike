5c49e0dcf80451283612bb462ee19404f6e508e1
Arkadiusz Lis <areklis909@gmail.com>
2019-12-24 10:43:32 +0100

    ConfigProcessor API polishing

--- removeTheSpike/include/ConfigProcessor.hpp
+++ removeTheSpike/include/ConfigProcessor.hpp
@@ -8,12 +8,32 @@ namespace NsConfigProcessor {
 class ConfigProcessor {
 
     libconfig::Config config;
+    const std::string modelOrderStr = "modelOrder";
+    const std::string roStr = "ro";
+    const std::string maxAlarmLengthStr = "maxAlarmLength";
+    const std::string miStr = "mi";
+    const std::string lambdaStr = "lambda";
+    const std::string lambdaZeroStr = "lambdaZero";
 
     template<typename T>
-    T readSetting(const std::string & settingName);
+    T readSetting(const std::string & settingName) {
+        return config.lookup(settingName);
+    }
 
     template<typename T>
-    void writeSetting(const std::string & settingName, const T & settingValue);
+    void writeSetting(const std::string & settingName, const T & settingValue) {
+        libconfig::Setting & root = config.getRoot();
+        libconfig::Setting::Type type = libconfig::Setting::TypeString;
+        if constexpr (std::is_same_v<T, std::string>) {
+            type = libconfig::Setting::TypeString;
+        } else if constexpr (std::is_same_v<T, int>) {
+            type = libconfig::Setting::TypeInt;
+        } else if constexpr (std::is_same_v<T, double>) {
+            type = libconfig::Setting::TypeFloat;
+        }
+
+        root.add(settingName, type) = settingValue;
+    }
 
 public:
     ConfigProcessor(const std::string & configName);
@@ -25,12 +45,14 @@ public:
     int readLambda();
     int readLambdaZero();
 
-    void writeModelOrder();
-    void writeRo();
-    void writeMaxAlarmLength();
-    void writeMi();
-    void writeLambda();
-    void writeLambdaZero();
+    void writeModelOrder(const int r);
+    void writeRo(const int ro);
+    void writeMaxAlarmLength(const int length);
+    void writeMi(const int mi);
+    void writeLambda(const int lambda);
+    void writeLambdaZero(const int lambdaZero);
+
+    void commit(const std::string & configName);
 
 };
 
--- removeTheSpike/src/ConfigProcessor.cpp
+++ removeTheSpike/src/ConfigProcessor.cpp
@@ -7,6 +7,63 @@ using namespace libconfig;
 
 namespace NsConfigProcessor {
 
+ConfigProcessor::ConfigProcessor(const std::string & configName) {
+    config.setOptions(Config::OptionSemicolonSeparators);
+    config.readFile(configName.c_str());
+}
+
+int ConfigProcessor::readModelOrder() {
+    return readSetting<int>(modelOrderStr);
+}
+
+int ConfigProcessor::readRo() {
+    return readSetting<int>(roStr);
+}
+
+int ConfigProcessor::readMaxAlarmLength() {
+    return readSetting<int>(maxAlarmLengthStr);
+}
+
+int ConfigProcessor::readMi() {
+    return readSetting<int>(miStr);
+}
+
+int ConfigProcessor::readLambda() {
+    return readSetting<int>(lambdaStr);
+}
+
+int ConfigProcessor::readLambdaZero() {
+    return readSetting<int>(lambdaZeroStr);
+}
+
+void ConfigProcessor::writeModelOrder(const int r) {
+    writeSetting(modelOrderStr, r);
+}
+
+void ConfigProcessor::writeRo(const int ro) {
+    writeSetting(roStr, ro);
+}
+
+void ConfigProcessor::writeMaxAlarmLength(const int length) {
+    writeSetting(maxAlarmLengthStr, length);
+}
+
+void ConfigProcessor::writeMi(const int mi) {
+    writeSetting(miStr, mi);
+}
+
+void ConfigProcessor::writeLambda(const int lambda) {
+    writeSetting(lambdaStr, lambda);
+}
+
+void ConfigProcessor::writeLambdaZero(const int lambdaZero) {
+    writeSetting(lambdaZeroStr, lambdaZero);
+}
+
+void ConfigProcessor::commit(const std::string & configName) {
+    config.writeFile(configName);
+}
+
 
 }
 
